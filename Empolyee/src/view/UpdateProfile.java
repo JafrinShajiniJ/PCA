957b170280ec8f1713ab9a5fdad5a778a4a0871b
JafrinShajiniJ <48121674+JafrinShajiniJ@users.noreply.github.com>
2021-06-14 17:53:49 +0530

    Update UpdateProfile.java

--- Empolyee/src/view/UpdateProfile.java
+++ Empolyee/src/view/UpdateProfile.java
@@ -1,18 +1,46 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
+import java.awt.event.*;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Component;
+import javax.swing.SwingConstants;
 import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.Cursor;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.JPasswordField;
+import controller.Employeecontroller;
+import daoimp.Employeedao;
+import java.sql.*;
+import java.sql.SQLException;
+import config.Jdbcconnection;
 
-public class UpdateProfile {
+public class UpdateProfile extends JFrame {
 
-	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JPasswordField passwordField;
+	private JPanel contentPane;
+	private JTextField tFirstName;
+	private JTextField tLastName;
+	private JTextField tUserId;
+	private JPasswordField tPassword;
+	private JPasswordField tConfirmPassword;
+	private JTextField tEmail;
+	private JTextField tRole;
+	private JTextField tGender;
+	
+	private static JFrame f;
+	private static Employeecontroller employeecontroller=null;
 
 	/**
 	 * Launch the application.
@@ -21,8 +49,8 @@ public class UpdateProfile {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					UpdateProfile window = new UpdateProfile();
-					window.frame.setVisible(true);
+					UpdateProfile frame = new UpdateProfile();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -31,49 +59,140 @@ public class UpdateProfile {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
-	public UpdateProfile() {
-		initialize();
-	}
+	public UpdateProfile() throws ClassNotFoundException, SQLException {
+		
+		f=new JFrame();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 757, 727);
+		contentPane = new JPanel();
+		contentPane.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+		//contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setBorder(new LineBorder(null, 1, true));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		JLabel lblNewLabel = new JLabel("Registration Form");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblNewLabel.setBounds(283, 31, 340, 33);
+		contentPane.add(lblNewLabel);
+		
+				
+		tFirstName = new JTextField();
+		tFirstName.setBounds(16, 140, 268, 33);
+		contentPane.add(tFirstName);
+		tFirstName.setColumns(10);
+		
+		tLastName = new JTextField();
+		tLastName.setColumns(10);
+		tLastName.setBounds(355, 140, 268, 33);
+		contentPane.add(tLastName);
+		
+		tUserId = new JTextField();
+		tUserId.setColumns(10);
+		tUserId.setBounds(16, 234, 268, 33);
+		contentPane.add(tUserId);
+		
+		tEmail = new JTextField();
+		tEmail.setBounds(355,234,268,33);
+      	tEmail.setColumns(10);
+		contentPane.add(tEmail);
+		
+		
+		JLabel LFirstName = new JLabel("First Name");
+		LFirstName.setFont(new Font("Apple Braille", Font.PLAIN, 15));
+		LFirstName.setBounds(16, 119, 109, 16);
+		contentPane.add(LFirstName);
+		
+		JLabel lLastName = new JLabel("Last Name");
+		lLastName.setFont(new Font("Apple Braille", Font.PLAIN, 15));
+		lLastName.setBounds(355, 119, 109, 16);
+		contentPane.add(lLastName);
+		
+		JLabel lUserId = new JLabel("User ID");
+		lUserId.setFont(new Font("Apple Braille", Font.PLAIN, 15));
+		lUserId.setBounds(16, 213, 109, 16);
+		contentPane.add(lUserId);
+		
+		// label: emailLabel
+		JLabel emailLabel = new JLabel("Email");
+		emailLabel.setFont(new Font("Apple Braille", Font.PLAIN, 15));
+		emailLabel.setBounds(355, 213, 109, 16);
+		contentPane.add(emailLabel);
+		
+		JLabel lPassword = new JLabel(" Create Password");
+		lPassword.setFont(new Font("Apple Braille", Font.PLAIN, 15));
+		lPassword.setBounds(16, 310, 268, 16);
+		contentPane.add(lPassword);
+		
+		JLabel lConfirmPassword = new JLabel("Confirm Password");
+		lConfirmPassword.setFont(new Font("Apple Braille", Font.PLAIN, 15));
+		lConfirmPassword.setBounds(355, 310, 268, 16);
+		contentPane.add(lConfirmPassword);
+		
+		JLabel lRole = new JLabel("Gender");
+		lRole.setFont(new Font("Apple Braille", Font.PLAIN, 15));
+		lRole.setBounds(355, 413, 109, 16);
+		contentPane.add(lRole);
+		
+ 		
+		
+		JLabel lGender = new JLabel("Role");
+		lGender.setFont(new Font("Apple Braille", Font.PLAIN, 15));
+		lGender.setBounds(16, 413, 109, 16);
+		contentPane.add(lGender);
+		
+		
+		JButton bSubmit = new JButton("Submit");
+		employeecontroller=new Employeecontroller();
+		bSubmit.addActionListener(new ActionListener() {
+			@Override
+			public  void actionPerformed(ActionEvent arg0) {
+				String UFN, ULN, UID, UE,UP,UR,UG, UCP;
+				UFN=tFirstName.getText();
+				ULN=tLastName.getText();
+				UID=tUserId.getText();
+				UE=tEmail.getText();
+				UP=new String(tPassword.getPassword());
+				UR=tRole.getText();
+				UG=tGender.getText();
+				UCP=new String(tConfirmPassword.getPassword());
+				if(UP.equals(UCP)) {
+					
+					employeecontroller.addEmployee(UFN, ULN, UID,UE, UP,UR, UG);
+					JOptionPane.showMessageDialog(f, "Registered Successfully...");
+						
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+				}
+				else {
+					JOptionPane.showMessageDialog(f, "Check your Password and Retry...");
+				}
+			}
+		});
+		bSubmit.setBackground(Color.LIGHT_GRAY);
+		bSubmit.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		bSubmit.setBounds(280, 622, 142, 40);
+		contentPane.add(bSubmit);
 		
-		JLabel lblNewLabel = new JLabel("EmpId");
-		lblNewLabel.setBounds(59, 50, 104, 26);
-		frame.getContentPane().add(lblNewLabel);
+		tPassword = new JPasswordField();
+		tPassword.setBounds(16, 331, 268, 33);
+		contentPane.add(tPassword);
 		
-		textField = new JTextField();
-		textField.setBounds(209, 54, 96, 19);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		tConfirmPassword = new JPasswordField();
+		tConfirmPassword.setBounds(355, 331, 268, 33);
+		contentPane.add(tConfirmPassword);
 		
-		JLabel lblNewLabel_1 = new JLabel("First Name");
-		lblNewLabel_1.setBounds(54, 105, 83, 13);
-		frame.getContentPane().add(lblNewLabel_1);
+		tRole = new JTextField();
+		tRole.setColumns(10);
+		tRole.setBounds(355, 441, 268, 33);
+		contentPane.add(tRole);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(209, 102, 96, 19);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		tGender = new JTextField();
+		tGender.setColumns(10);
+		tGender.setBounds(16, 441, 268, 33);
+		contentPane.add(tGender);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setBounds(59, 154, 45, 13);
-		frame.getContentPane().add(lblNewLabel_2);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(209, 151, 96, 19);
-		frame.getContentPane().add(passwordField);
-		
-		JLabel lblNewLabel_3 = new JLabel("Update Profile");
-		lblNewLabel_3.setBounds(127, 10, 145, 13);
-		frame.getContentPane().add(lblNewLabel_3);
 	}
 }
